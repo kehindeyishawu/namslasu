70a115eead7d8a9f090f95baa29483c7ecfa1285
kenny <fkennyf@gmail.com>
2020-06-17 09:32:49 +0000

    added passport package

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express"),
 	  app = express(),
 	  bodyParser = require("body-parser"),
 	  mongoose = require("mongoose"),
+	  passport  = require("passport"),
+	  LocalStrategy = require("passport-local"),
 	  methodOverride = require("method-override");
 
 // Database config
